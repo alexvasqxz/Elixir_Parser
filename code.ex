cf9a137fc97a8449066716b73b1607d63c2f5e81
Gus Vasquez <gusvasquez@Guss-MacBook-Pro.local>
2022-06-04 00:09:11 -0500

    Deliverable

---
+++ code.ex
@@ -0,0 +1,44 @@
+# The Greatest ParsElixir
+#
+# Brenda Giselle Molina Escamilla
+# Gustavo Alejandro Vasquez Acosta
+# Nicolás Gazzolo Varela
+#
+# June 2nd, 2022
+
+defmodule Parser do
+
+  # This function will create the dictionary
+  # that contains the keywords for our language
+  def keyWords(input) do
+
+  end
+
+  # This function will open the file and returns
+  # its contents in a list (list of lines)
+  def read_contents(path) do
+
+  end
+
+  # This function will process each line,
+  # to find keywords and in that case call
+  # the append function
+  def process_line(line) do
+
+  end
+
+  # This function will append the
+  # style and spaces to the lines that
+  # have keywords
+  def append(line) do
+
+  end
+
+  # This function will store the results we
+  # will be collecting into our
+  # output file
+  def store_contents(file) do
+
+  end
+
+end
